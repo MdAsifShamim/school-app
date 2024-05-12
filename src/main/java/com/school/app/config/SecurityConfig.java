175ede3bebe51e4fda76a317a9dfc0ad821336ac
Admin <Admin@DESKTOP-A7QRCFU>
2024-05-12 15:10:40 +0530

    Comment: Initial Commit with default security

---
+++ src/main/java/com/school/app/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.school.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import  org.springframework.security.config.Customizer;
+
+@Configuration
+public class SecurityConfig {
+
+	
+	@Bean
+	SecurityFilterChain customeSecurityFilter(HttpSecurity httpSequrity) throws Exception{
+		httpSequrity
+		.csrf(csrf->csrf.disable())
+		.authorizeHttpRequests(request->request.requestMatchers("/register").permitAll());
+		httpSequrity.formLogin(Customizer.withDefaults());
+		httpSequrity.httpBasic(Customizer.withDefaults());
+		return httpSequrity.build();
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
